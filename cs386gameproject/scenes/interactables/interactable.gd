e3aefb13330fb03193ff49d59a6b58fec2be33ac
AsherSSB <gromanenghi04@gmail.com>
2025-02-19 00:06:03 -0700

    fixed merge

---
+++ cs386gameproject/scenes/interactables/interactable.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@export var interact_name: String = ""
+@export var is_interactable: bool = true
+# triggering this from the interaction component
+var interact: Callable = func():
+	pass
