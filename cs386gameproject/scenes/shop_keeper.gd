396b5bf4f9585e22e5396594de2cc9b12787e8d3
Elizabeth <elizabethbooth05@gmail.com>
2025-02-18 23:51:39 -0700

    Added collision detection for shopkeeper and prompt for user with shop basic functionality (#66)
    * Added collision detection for shopkeeper and prompt for user
    
    * Added inventory basic with one health potion. When pressing e next to the shopkeeper it shows up pressing esc it goes away.

--- cs386gameproject/scenes/shop_keeper.gd
+++ cs386gameproject/scenes/shop_keeper.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @onready var animationShop = $AnimatedSprite2D
+@onready var interactable: Area2D = $Interactable
 
 func _ready():
 	animationShop.play("idleAnimation")
+
+	
