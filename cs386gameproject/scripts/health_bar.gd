4e8990a542aefd405f8744d7f18417db824d00fd
AsherSSB <130717471+AsherSSB@users.noreply.github.com>
2025-02-21 00:19:33 -0700

    Display damage (#75)
    * added floating damage displays
    
    * successfully merged

--- cs386gameproject/scripts/health_bar.gd
+++ cs386gameproject/scripts/health_bar.gd
@@ -1,5 +1,7 @@
 extends ProgressBar
 
+static var DamageDisplay = preload("res://scenes/display_damage.tscn")
+
 @onready var player: CharacterBody2D = $"../../Player"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,4 +9,7 @@ func _ready() -> void:
 	player.health_update.connect(update)
 	
 func update():
+	var display = DamageDisplay.instantiate()
+	display.set_data(value - player.health, player.global_position, get_tree().root)
+	display.display()
 	value = player.health
