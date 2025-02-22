ece0a26e6ae9eb060cb9aee2cc52c9821e984cd2
ethanSchoolAccount <edm266@nau.edu>
2025-02-22 10:17:47 -0700

    Updated coins
    Made it so that there is a total accumulation of coins. Display is shown at all times with the total, with a display below that will only appear if coins are being collected. there is the variable "cumulative_coin_total" that can be used for later vendor, buying and selling uses, holding the total amount of coins the player has.

--- cs386gameproject/scripts/coin.gd
+++ cs386gameproject/scripts/coin.gd
@@ -1,50 +1,30 @@
 extends Area2D
 
-@onready var main = get_node("/root/MainScene")
 @onready var player = get_node("/root/MainScene/Player")
-@onready var bronze_label = get_node("/root/MainScene/PlayerUI/Coins/bronze_coin")
-@onready var silver_label = get_node("/root/MainScene/PlayerUI/Coins/silver_coin")
-@onready var gold_label = get_node("/root/MainScene/PlayerUI/Coins/gold_coin")
-
-var coin_type : int # 0: bronze, 1: silver, 2: gold
 
 const bronze_coin = preload("res://assets/bronze-coin.png")
 const silver_coin = preload("res://assets/silver-coin.png")
 const gold_coin = preload("res://assets/gold-coin.png")
 
 var textures = [bronze_coin, silver_coin, gold_coin]
+var coin_type : int = 0  # The coin type can still affect appearance if desired.
+
+var bronze_amount = 1
+var silver_amount = 3
+var gold_amount = 5
 
 func _ready() -> void:
 	$Sprite2D.texture = textures[coin_type]
 
 func _on_body_entered(body: Node2D) -> void:
-	#bronze
+	# Instead of doing any conversion, simply add one coin to the total.
 	if coin_type == 0:
-		print("bronze coin")
-		player.bronze_coins += 1
-		if player.bronze_coins == 15:
-			player.bronze_coins -= 15
-			player.silver_coins += 1
-			if player.silver_coins == 15:
-				player.silver_coins -= 15
-				player.gold_coins += 1
-				gold_label.text = "Gold Coins: " + str(player.gold_coins)
-			silver_label.text = "Silver Coins: " + str(player.silver_coins)
-		bronze_label.text = "Bronze Coins: " + str(player.bronze_coins)
-		print("Bronze Coins: " + str(player.bronze_coins)) # debug
-	elif coin_type == 1:
-		print("silver coin")
-		player.silver_coins += 1
-		if player.silver_coins == 15:
-			player.silver_coins -= 15
-			player.gold_coins += 1
-			gold_label.text = "Gold Coins: " + str(player.gold_coins)
-		silver_label.text = "Silver Coins: " + str(player.silver_coins)
-		print("Silver Coins: " + str(player.silver_coins)) # debug
-	elif coin_type == 2:
-		print("gold coin")
-		player.gold_coins += 1
-		gold_label.text = "Gold Coins: " + str(player.gold_coins)
-		print("Gold Coins: " + str(player.gold_coins)) # debug
-
+		player.add_coins(bronze_amount, 0)
+		print("Total coins: " + str(player.cumulative_coin_total))
+	if coin_type == 1:
+		player.add_coins(silver_amount, 1)
+		print("Total coins: " + str(player.cumulative_coin_total))
+	if coin_type == 2:
+		player.add_coins(gold_amount, 2)
+		print("Total coins: " + str(player.cumulative_coin_total))
 	queue_free()
