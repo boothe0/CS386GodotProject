636bd03e6e8a90d7d3b14404b65f864b37078c53
AsherSSB <gromanenghi04@gmail.com>
2025-02-27 16:37:26 -0700

    added player variables singleton and stat update function to player

--- cs386gameproject/scripts/emitter.gd
+++ cs386gameproject/scripts/emitter.gd
@@ -4,18 +4,10 @@ signal consumable_1
 signal consumable_2
 signal consumable_3
 signal round_end
+signal player_variables_updated
 
 signal buy_item_pressed(
 	item_name: String, price: int, shop_spot: int, consumable: bool
 )
 
 signal remote_shop_item(shop_spot: int)
-
-func debug():
-	print("debug pressed")
-	update_sword_scale(2.0)
-	
-func update_sword_scale(modifier: float):
-	var player = $"../Player"
-	var sword = player.get_node("Sword")
-	sword.scale = Vector2(sword.scale.x * modifier, sword.scale.y * modifier)
--- cs386gameproject/scripts/upgrades/increase_sword_size.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-@onready var sword = $Sword
-
-func apply(modifier:float):
-	sword.scale = Vector2(sword.scale.x * modifier, sword.scale.y * modifier)
