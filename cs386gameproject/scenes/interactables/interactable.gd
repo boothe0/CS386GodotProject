f922ac825412710d1dcf9ac4672ae6efbc5cd968
boothe0 <elizabethbooth05@gmail.com>
2025-02-28 10:31:47 -0700

    Added to the interactable script to have consumables spawn when entering the shop a random generator and more consumables can be added here

--- cs386gameproject/scenes/interactables/interactable.gd
+++ cs386gameproject/scenes/interactables/interactable.gd
@@ -1,7 +1,18 @@
 extends Area2D
 
+@onready var table_collision: CollisionShape2D = $TableCollision
+@onready var texture_rect: TextureRect = $TableCollision/TextureRect
+
 @export var interact_name: String = ""
 @export var is_interactable: bool = true
+# add more consumables to spawn here
+const HP_TEXTURE = "res://assets/VariPixels PotionsPack 02112022 Update/hpPotion.png"
+
 # triggering this from the interaction component
 var interact: Callable = func():
 	print("interaction triggered")
+
+		
+func _ready() -> void:
+	# loading texture
+	texture_rect.texture = load(HP_TEXTURE)
