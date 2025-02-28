bf88be4cf685e6a4d987b42a01dca8fe5a7628de
boothe0 <elizabethbooth05@gmail.com>
2025-02-28 10:05:59 -0700

    Fixed weird behavior in the shop scene from before

--- cs386gameproject/scenes/interactables/interacting_component.gd
+++ cs386gameproject/scenes/interactables/interacting_component.gd
@@ -12,36 +12,35 @@ func _ready():
 	scene_instantiate = load("res://assets/UI_Elements/shop_screen_ui.tscn")
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact") and label_showing == true:
-		if not is_scene_instantiated:
+	if event.is_action_pressed("interact") and can_interact:
+		if current_interactions: 
 			instantiated_scene = scene_instantiate.instantiate()
 			get_parent().add_child(instantiated_scene)
 			is_scene_instantiated = true
-	if is_scene_instantiated:  
-		if Input.is_action_just_pressed("escape"):
-			instantiated_scene.queue_free()
-			is_scene_instantiated = false
-		if current_interactions:
 			can_interact = false
 			interact_label.hide()
 			await current_interactions[0].interact.call()
-
 			can_interact = true
-
+	if event.is_action_pressed("escape") and is_scene_instantiated == true:
+			instantiated_scene.queue_free()
+			is_scene_instantiated = false
+		
 func _process(_delta: float) -> void:
 	var i = 0
 	if current_interactions and can_interact:
-		if current_interactions[i].is_interactable:
-			interact_label.text = current_interactions[0].interact_name
+		# returns T or F if an object should be in front or behind in the array
+		current_interactions.sort_custom(_sort_by_nearest)
+		if current_interactions[0].is_interactable:
+			interact_label.text   = current_interactions[0].interact_name
 			interact_label.show()
-			label_showing = true
-			i += 1 
-		else:
-			interact_label.hide()
-			label_showing = false
 	else:
 		interact_label.hide()
 		
+func _sort_by_nearest(area1, area2):
+	var area1_dist = global_position.distance_to(area1.global_position)
+	var area2_dist = global_position.distance_to(area2.global_position)
+	return area1_dist < area2_dist
+
 func _on_interacting_range_area_entered(area: Area2D) -> void:
 	current_interactions.push_back(area)
 
