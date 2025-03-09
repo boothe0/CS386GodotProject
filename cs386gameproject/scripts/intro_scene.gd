641b7c97ef99e0fe505d4219cec4b923599f0262
Camelia Mosor <crm782@nau.edu>
2025-03-08 21:59:50 -0700

    Fixed issue #125

--- cs386gameproject/scripts/intro_scene.gd
+++ cs386gameproject/scripts/intro_scene.gd
@@ -1,8 +1,8 @@
-extends Node2D
+extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Intro/Control/ContinueButton.grab_focus()
+	$CanvasLayer/ContinueButton.grab_focus()
 
 func _on_continue_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
