5f22cad13a32c1de2d61465e1df1e87f3408f6b0
boothe0 <elizabethbooth05@gmail.com>
2025-03-17 22:14:21 -0700

    Added the ui to transfer to the shop scene

--- cs386gameproject/scenes/shop_scene.gd
+++ cs386gameproject/scenes/shop_scene.gd
@@ -1,12 +1,7 @@
 extends Node2D
 @onready var coins_to_buy: Label = $Player/coins_to_buy
-
+var player_ui = load("res://assets/UI_Elements/player_ui.tscn")
 
 func _ready() -> void:
-	Emitter.buy_item_pressed.connect(_on_buy_item_pressed)
-	update_label()
-
-func update_label():
-	coins_to_buy.text = "Total Coins: " + str(PlayerVariables.coins)	
-func _on_buy_item_pressed(item_name: String, price: int, shop_spot: int, consumable: bool):
-	update_label()
+	var ui_scene = player_ui.instantiate()
+	add_child(ui_scene)
--- cs386gameproject/scripts/round_timer.gd
+++ cs386gameproject/scripts/round_timer.gd
@@ -1,16 +1,18 @@
 extends Label
 
 var round_over = false
+var time
 @onready var timer = $"../../RoundTimer"
 
 
 func _process(delta: float) -> void:
 	# called every frame. 'delta' is the elapsed time since the previous frame.
-	var time = int(timer.get_time_left())
-	self.set_text(str(time))
-	
-	# end round when time is up
-	if time == 0 and round_over == false:
-		round_over = true
-		PlayerVariables.rounds += 1
-		Emitter.emit_signal("round_end")
+	if timer:
+		time = int(timer.get_time_left())
+		self.set_text(str(time))
+		
+		# end round when time is up
+		if time == 0 and round_over == false:
+			round_over = true
+			PlayerVariables.rounds += 1
+			Emitter.emit_signal("round_end")
