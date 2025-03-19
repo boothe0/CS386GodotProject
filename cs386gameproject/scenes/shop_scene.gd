6a3b32cc75182a3e672d0da97e5169fd77e30b01
AsherSSB <gromanenghi04@gmail.com>
2025-03-18 21:50:18 -0700

    Removed orphaned buy_item signal from legacy interaction system

--- cs386gameproject/scenes/shop_scene.gd
+++ cs386gameproject/scenes/shop_scene.gd
@@ -3,11 +3,7 @@ extends Node2D
 
 
 func _ready() -> void:
-	Emitter.buy_item_pressed.connect(_on_buy_item_pressed)
 	update_label()
 
 func update_label():
-	coins_to_buy.text = "Total Coins: " + str(PlayerVariables.coins)	
-func _on_buy_item_pressed(item_name: String, price: int, shop_spot: int, consumable: bool):
-	update_label()
-
+	coins_to_buy.text = "Total Coins: " + str(PlayerVariables.coins)
