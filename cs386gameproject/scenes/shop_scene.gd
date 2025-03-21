2fcbace501c64f49cfc0a9eee964978d76ef078c
boothe0 <elizabethbooth05@gmail.com>
2025-03-20 19:21:03 -0700

    Scene transfers between shop and main scene removed label functions in shop_scene and added to new player_ui script to update label

--- cs386gameproject/scenes/shop_scene.gd
+++ cs386gameproject/scenes/shop_scene.gd
@@ -1,15 +1,6 @@
 extends Node2D
-@onready var coins_to_buy: Label = $Player/coins_to_buy
 
 
 func _ready() -> void:
 	InputMap.action_erase_events("attack")
 	InputMap.action_erase_events("ranged_attack")
-	Emitter.buy_item_pressed.connect(_on_buy_item_pressed)
-	update_label()
-
-func update_label():
-	coins_to_buy.text = "Total Coins: " + str(PlayerVariables.coins)
-	
-func _on_buy_item_pressed(item_name: String, price: int, shop_spot: int, consumable: bool):
-	update_label()
