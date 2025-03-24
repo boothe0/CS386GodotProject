8201cb9cf1ecc20a08127776fb542a08cdff5e04
boothe0 <elizabethbooth05@gmail.com>
2025-03-24 13:45:52 -0700

    HP potions dynamically add to the inventory between shopscene and round not yet from round to shopscene

--- cs386gameproject/scripts/upgrades/player_variables.gd
+++ cs386gameproject/scripts/upgrades/player_variables.gd
@@ -1,6 +1,6 @@
 extends Node
 # Called when the node enters the scene tree for the first time.
-@onready var coins: int = 25
+@onready var coins: int = 50
 @onready var rounds: int = 0
 
 @onready var health_scale: float = 1.0
@@ -9,3 +9,24 @@ extends Node
 @onready var sword_scale: float = 1.0
 
 @onready var projectile_scale: float = 1.0
+
+@onready var consumables: Array[Dictionary] = [
+	{
+		"texture" : "res://assets/VariPixels PotionsPack 02112022 Update/hpPotion.png",
+		"script" : "res://scenes/consumables/heal_potion.tscn"
+	},
+	{
+		"texture" : "res://assets/VariPixels PotionsPack 02112022 Update/WallPotion.png",
+	},
+]
+
+@onready var consumable_scale_x = 3
+@onready var consumable_scale_y = 3.4
+@onready var consumable_size = Vector2(50, 40)
+# consumable 1 slot positions
+@onready var consumable_1_pos = Vector2(960,0)
+# consumable 2 slot positions
+@onready var consumable_2_pos = Vector2(960,150)
+# consumable 3 slot positions
+@onready var consumable_3_pos = Vector2(960,300)
+@onready var number_consumables : int = 0
