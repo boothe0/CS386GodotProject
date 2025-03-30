c23c779a6aeebc4925bb64f3834394f6184b616d
boothe0 <elizabethbooth05@gmail.com>
2025-03-29 21:48:40 -0700

    Feel it is in a good enough state

--- cs386gameproject/scenes/player_ui.gd
+++ cs386gameproject/scenes/player_ui.gd
@@ -17,6 +17,7 @@ extends CanvasLayer
 
 @onready var total_coins: Label = $Coins/HBoxContainer/TotalCoins
 @onready var number_consumables_left = 0
+# array to return the script and the texture from display
 var scripts_and_texture = []
 var texture
 var script_object
@@ -31,7 +32,7 @@ func _ready() -> void:
 	if round >= 1:
 		refresh_ui()
 func decrement_num_consumables(num_consumabless):
-	number_consumables_left -= 1
+	PlayerVariables.number_consumables_left -= 1
 
 func update_label():
 	total_coins.text = "Total Coins: " + str(PlayerVariables.coins)
@@ -40,63 +41,57 @@ func _on_buy_item_pressed(item_name: String, price: int, shop_spot: int, consuma
 	buy_ui_consumables()
 	update_label()
 
+# searches for the display the player interacted with
 func search_display_texture_signal():
 	for item in display_items:
 		if item and item.in_range:
 			var texture = item.texture
 			var shop_script_node = item.get_node("TableCollision/Control/ShopItem")
 			var script = shop_script_node.item_script
-			return texture
+			return [texture, script]
 
-
-# load only once
-func buy_ui_consumables():
-	var b = consumable_base.instantiate()
-	var texture_rect = b.get_node("TextureRect")
-	b.set_script(consumable_1)
-	b.size = PlayerVariables.consumable_size
-	b.scale.x = PlayerVariables.consumable_scale_x
-	b.scale.y = PlayerVariables.consumable_scale_y
-	if PlayerVariables.number_consumables == 0:
-		texture = search_display_texture_signal()
-		b.get_child(0).text = "Z"
+# removes repetative code and attaches texture and script to consumable
+func itermediate_loading(b):
+		var texture_rect = b.get_node("TextureRect")
+		scripts_and_texture = search_display_texture_signal()
+		texture = scripts_and_texture[0]
+		script_object = scripts_and_texture[1]
 		b.position = PlayerVariables.consumable_1_pos
 		PlayerVariables.consumables[0]["position"] = b.position
 		PlayerVariables.consumables[0]["script"] = script_object
 		PlayerVariables.consumables[0]["texture"] = texture
 		texture_rect.texture = load(PlayerVariables.consumables[0]["texture"])
-		print(PlayerVariables.consumables[0]["position"])
+
+# call in the shop scene
+func buy_ui_consumables():
+	var b = consumable_base.instantiate()
+	b.set_script(consumable_1)
+	b.size = PlayerVariables.consumable_size
+	b.scale.x = PlayerVariables.consumable_scale_x
+	b.scale.y = PlayerVariables.consumable_scale_y
+	if PlayerVariables.number_consumables == 0:
+		itermediate_loading(b)
+		b.get_child(0).text = "Z"
+		control.add_child(b)
 
 	elif PlayerVariables.number_consumables == 1:
-		texture = search_display_texture_signal()
+		itermediate_loading(b)
 		b.get_child(0).text = "X"
-		b.position = PlayerVariables.consumable_2_pos
-		PlayerVariables.consumables[1]["position"] = b.position
-		PlayerVariables.consumables[1]["script"] = script_object
-		PlayerVariables.consumables[1]["texture"] = texture
-		texture_rect.texture = load(PlayerVariables.consumables[1]["texture"])
+		control.add_child(b)
 	else:
-		texture = search_display_texture_signal()
+		itermediate_loading(b)
 		b.get_child(0).text = "C"
-		b.position = PlayerVariables.consumable_3_pos
-		PlayerVariables.consumables[2]["position"] = b.position
-		PlayerVariables.consumables[2]["script"] = script_object
-		PlayerVariables.consumables[2]["texture"] = texture
-		texture_rect.texture = load(PlayerVariables.consumables[2]["texture"])
-	PlayerVariables.number_consumables += 1
-	control.add_child(b)
+		control.add_child(b)
 
+# supposed to check the position and assign appropriate label on reload
 func check_position_reload(b, reload_number):
-	# since the dictionary is checked backwards you need to count from 2-1-0
-	# 2 being the first so it correlates to Z
-	print(reload_number)
-	if reload_number == 1:
+	if reload_number == 0:
 		return "Z"
-	elif reload_number == 2:
+	elif reload_number == 1:
 		return "X"
 	else:
 		return "C"
-
+# reloads the ui
 func refresh_ui():
 	var items_to_reload_left = PlayerVariables.number_consumables
 	for item in PlayerVariables.consumables: 
