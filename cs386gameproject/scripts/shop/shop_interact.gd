a599c59391e7e9e4f7f21c3b14103bf8a9c7244d
boothe0 <elizabethbooth05@gmail.com>
2025-04-18 01:02:40 -0700

    added buy item sound effect

--- cs386gameproject/scripts/shop/shop_interact.gd
+++ cs386gameproject/scripts/shop/shop_interact.gd
@@ -2,11 +2,13 @@ extends Area2D
 @onready var item := $TableCollision/Control/ShopItem
 var in_range = false
 @onready var label: Label = $CollisionShape2D/Label
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("interact") and in_range:
 		if item.buy_item_pressed(): # successfully bought item
+			audio_stream_player_2d.play()
 			self.set_script(null) # detach script from node
 
 func _on_body_entered(body: Node2D) -> void:
