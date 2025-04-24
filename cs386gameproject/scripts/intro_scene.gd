3eba1ac26a0b1866a8a0e5d0ac137880cd0e5c87
AsherSSB <gromanenghi04@gmail.com>
2025-04-24 13:26:02 -0700

    player variables now reset on player clicking continue on intro screen

--- cs386gameproject/scripts/intro_scene.gd
+++ cs386gameproject/scripts/intro_scene.gd
@@ -5,4 +5,6 @@ func _ready() -> void:
 	$CanvasLayer/ContinueButton.grab_focus()
 
 func _on_continue_button_pressed() -> void:
+	PlayerVariables.set_script(null)
+	PlayerVariables.set_script(preload("res://scripts/upgrades/player_variables.gd"))
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
