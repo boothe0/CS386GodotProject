d7241914f88898c566c87001c6a8bb6dcafe6e3c
AsherSSB <gromanenghi04@gmail.com>
2025-04-26 10:45:35 -0700

    fixed player variables being all set to 0 after script re-attachment

--- cs386gameproject/scripts/upgrades/player_variables.gd
+++ cs386gameproject/scripts/upgrades/player_variables.gd
@@ -1,11 +1,11 @@
 extends Node
 # Called when the node enters the scene tree for the first time.
-@onready var coins: int = 25
-@onready var rounds: int = 0
+var coins: int = 25
+var rounds: int = 0
 
-@onready var health_scale: float = 1.0
+var health_scale: float = 1.0
 
-@onready var sword_damage_modifier: float = 1.0
-@onready var sword_scale: float = 1.0
+var sword_damage_modifier: float = 1.0
+var sword_scale: float = 1.0
 
-@onready var projectile_scale: float = 1.0
+var projectile_scale: float = 1.0
