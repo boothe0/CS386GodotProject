4e838878f8c2f139f9c7b8b373d91759eb04c307
Camelia Mosor <crm782@nau.edu>
2025-04-28 08:04:05 -0700

    Updated center objective

--- cs386gameproject/scripts/center_objective.gd
+++ cs386gameproject/scripts/center_objective.gd
@@ -6,11 +6,13 @@ var health: int
 @onready var sprite := $Sprite2D
 @onready var area_detection = $Area2D
 @onready var damage_timer = $DamageTimer
+@onready var shadow = $Shadow
 
 var enemies: int
 
 func _ready():
 	health = max_health
+	shadow.self_modulate.a = 0;
 	connect("body_entered", _on_area_2d_body_entered)
 	connect("body_exited", _on_area_2d_body_exited)
 	damage_timer.timeout.connect(_on_SpawnTimer_timeout)
@@ -18,10 +20,13 @@ func _ready():
 	
 func take_damage(amount: int):
 	health -= amount
+	sprite.frame = 11 - health
+	shadow.self_modulate.a = 1 - 0.1 * health
 	print("objective Health: %d" % health)
 	if health <= 0:
 		queue_free()
 		print("Objective destroyed!")
+		get_tree().change_scene_to_file("res://scenes/death.tscn")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name != "Player":
